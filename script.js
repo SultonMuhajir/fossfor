3f8ce49f0e658848812f9df0081f63ac11b65031
SultonMuhajir <sulton.muhajir28@gmail.com>
2021-02-06 17:58:41 +0700

    fossfor v1.0

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 // => Window on Scroll
 $(window).on("scroll", function () {
    if ($(this).scrollTop() >= 2) {
-      $("nav").css({
-         "backgroundColor": "#eee",
-         "boxShadow": "0 1px 10px rgba(0,0,0,.3)"
-      })
-      $("nav a").css("color", "black");
+      $("nav").css("backgroundColor", "#222");
       $("nav ul").css("borderRadius", "0");
-      $("nav span").css("backgroundColor", "black");
    } else {
-      $("nav").css({
-         "backgroundColor": "",
-         "boxShadow": ""
-      })
-      $("nav a").css("color", "");
+      $("nav").css("backgroundColor", "initial");
       $("nav ul").css("borderRadius", "15px 0 0 0");
-      $("nav span").css("backgroundColor", "");
    }
 });
 
@@ -29,16 +19,6 @@ $("nav li a").on("click", function (e) {
          $("nav li").eq(i).removeClass("liAction");
       }, 150 * (i + 1));
    });
-
-   const tujuan = $(this).attr("href");
-   const yOffset = -40;
-   const element = $(tujuan)[0];
-   const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-   window.scrollTo({
-      top: y,
-      behavior: 'smooth'
-   });
-   e.preventDefault();
 });
 
 
